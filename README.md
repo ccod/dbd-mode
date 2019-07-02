c31c90b4a03a4a29e7e03a3031659436209538d6
Christopher Codrington <chcodrington@gmail.com>
2019-07-02 15:30:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### DBDiagrams-mode
+
+## What
+[dbdiagrams.io](https://dbdiagrams.io) uses a dsl to describe entities which it then visualises.
+This is a major mode that runs off of `.dbd` files that tries to emulate the editing experience of the dsl.
+
+## Why?
+As much as I am enamored with the solution, I still find the editing experience from emacs(spacemacs) preferable
+to the online editor, as I can't get my vim bindings in the text box.
+
+## Installation
+This is a bit hacky as I am still rather unfamiliar with the right way to publish elisp code.
+`cd ~/.emacs.d/private/local`
+`git clone https://github.com/ccod/dbd-mode`
+`edit ~/.spacemacs`
+
+within `dotspacemacs/user-config`
+add `(load-file "~/.emacs,d/private/local/dbd-mode/dbdiagram-mode.el")`
+
+![demo of lang-mode](https://github.com/ccod/dbd-mode/syntax-demonstration.png)
+
+## TODO:
+- hook into the autocomplete mode
+- hook into flycheck to point out syntax errors
---
+++ dbdiagram-mode.el
@@ -0,0 +1,31 @@
+(defconst dbdiagram-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; / is punctuation, but // is a comment starter
+    (modify-syntax-entry ?/ ". 12" table)
+    ;; \n is a comment ender
+    (modify-syntax-entry ?\n ">" table)
+    table))
+
+(setq dbdiagram-font-lock-keywords
+      (let* ((x-types '("int" "integer" "char" "varchar" "bool" "boolean"
+                        "string" "text" "timestamp" "date"))
+             (x-fn-names  '("Table" "Ref" "ref" "Enum" "note"))
+             (x-keywords '("not" "null" "pk" "primary key" "unique"))
+
+             (x-types-regexp (regexp-opt x-types 'words))
+             (x-keywords-regexp (regexp-opt x-keywords 'words))
+             (x-fn-regexp (regexp-opt x-fn-names 'words)))
+
+        `((,x-types-regexp . font-lock-type-face)
+          (,x-keywords-regexp . font-lock-keyword-face)
+          (,x-fn-regexp . font-lock-function-name-face)
+          (">" . font-lock-function-name-face))))
+
+(define-derived-mode dbdiagram-mode python-mode
+  "dbdiagram.io erd definition mode"
+  :syntax-table dbdiagram-mode-syntax-table
+  (setq font-lock-defaults '((dbdiagram-font-lock-keywords)))
+  (setq-local python-indent 2)
+  (font-lock-fontify-buffer))
+
+(add-to-list 'auto-mode-alist '("\\.dbd\\'" . dbdiagram-mode))
